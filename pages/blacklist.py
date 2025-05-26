23a6737c121f0c563fb1758795eb3194b18c8c0a
jssoleey <155762388+jssoleey@users.noreply.github.com>
2025-05-26 16:16:33 +0900

    Update blacklist.py

--- pages/blacklist.py
+++ pages/blacklist.py
@@ -17,6 +17,8 @@ st.markdown("""
 
 if "user_name" not in st.session_state or "user_folder" not in st.session_state:
     st.warning("로그인 정보가 없습니다. 메인 페이지로 돌아가 로그인해 주세요.")
+    if st.button("🔐 로그인 페이지로 이동"):
+        st.switch_page("main.py")
     st.stop()
 
 DATA_DIR = "/data/blacklist_data"
