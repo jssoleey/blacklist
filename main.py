c259a9e67f9a6ea1c6f04d02f9cb1a922e3776d5
jssoleey <155762388+jssoleey@users.noreply.github.com>
2025-05-29 15:40:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # main.py
 import streamlit as st
+import os
+st.set_page_config(page_title="블랙리스트 관리", layout="wide", initial_sidebar_state="collapsed")
+st.write("현재 작업 디렉토리:", os.getcwd())
 st.switch_page("blacklist.py")
